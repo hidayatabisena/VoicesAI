4ff94a32b30ea9bfbef8da5ac6a299d6da90e6c5
Han Sena <abisena@outlook.com>
2024-04-24 13:18:12 +0700

    Initial Commit

---
+++ VoicesAI/VoicesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesAIApp.swift
+//  VoicesAI
+//
+//  Created by Hidayat Abisena on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
