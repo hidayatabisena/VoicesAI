527f555cf143c43efb024fcb06c3295fa59cca30
Han Sena <abisena@outlook.com>
2024-04-25 14:43:58 +0700

    Create SpeechVM to generate and play audio data

---
+++ VoicesAI/ViewModel/SpeechVM.swift
@@ -0,0 +1,55 @@
+//
+//  SpeechVM.swift
+//  VoicesAI
+//
+//  Created by Hidayat Abisena on 25/04/24.
+//
+
+import Foundation
+import AVFoundation
+
+@MainActor
+class SpeechVM: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    @Published var isLoading = false
+    @Published var isPlaying = false
+    @Published var errorMessage: String?
+    
+    private var elevenLabsService: ElevenLabsService?
+    private var audioPlayer: AVAudioPlayer?
+    
+    // MARK: - GENERATE AND PLAY SPEECH
+    func generateAndPlaySpeech(from text: String, apiKey: String) async {
+        isLoading = true
+        defer { isLoading = false }
+        defer { isPlaying = false }
+        errorMessage = nil
+        
+        elevenLabsService = ElevenLabsService(apiKey: apiKey)
+        
+        do {
+            guard let audioData = try await elevenLabsService?.generateSpeech(for: text) else {
+                errorMessage = "Failed to generate audio data."
+                print(errorMessage!)
+                return
+            }
+            
+            playAudio(from: audioData)
+        } catch {
+            print("Error generating or playing speech: \(error)")
+        }
+    }
+}
+
+// MARK: - FUNCTION PLAY AUDIO
+extension SpeechVM {
+    func playAudio(from data: Data) {
+        do {
+            audioPlayer = try AVAudioPlayer(data: data)
+            audioPlayer?.delegate = self
+            isPlaying = true
+            audioPlayer?.play()
+        } catch {
+            print("Audio player error: \(error)")
+        }
+    }
+}
